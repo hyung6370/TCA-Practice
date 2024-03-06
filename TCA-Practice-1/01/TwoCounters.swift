cdcbb65d10773bc067f15b4fb1af4e5b44332161
emptyhead <korea6370@yu.ac.kr>
2024-03-06 11:51:47 +0900

    24/3/6 update

---
+++ TCA-Practice-1/01/TwoCounters.swift
@@ -0,0 +1,18 @@
+//
+//  TwoCounters.swift
+//  TCA-Practice-1
+//
+//  Created by KIM Hyung Jun on 3/6/24.
+//
+
+import SwiftUI
+
+struct TwoCounters: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TwoCounters()
+}
