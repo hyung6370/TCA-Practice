276ef15e554a630f65236457c46f47fa8beecacf
emptyhead <korea6370@yu.ac.kr>
2024-03-06 11:51:57 +0900

    24/3/6 update

--- TCA-Practice-1/01/TwoCounters.swift
+++ TCA-Practice-1/01/TwoCounters.swift
@@ -6,13 +6,55 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
-struct TwoCounters: View {
+@Reducer
+struct TwoCounters {
+    @ObservableState
+    struct State: Equatable {
+        var counter1 = Counter.State()
+        var counter2 = Counter.State()
+    }
+    
+    enum Action {
+        case counter1(Counter.Action)
+        case counter2(Counter.Action)
+    }
+    
+    var body: some Reducer<State, Action> {
+        Scope(state: \.counter1, action: \.counter1) {
+            Counter()
+        }
+        Scope(state: \.counter2, action: \.counter2) {
+            Counter()
+        }
+    }
+}
+
+struct TwoCounterView: View {
+    let store: StoreOf<TwoCounters>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            HStack {
+                Text("Counter 1")
+                Spacer()
+                CounterView(store: store.scope(state: \.counter1, action: \.counter1))
+            }
+            
+            HStack {
+                Text("Counter 2")
+                Spacer()
+                CounterView(store: store.scope(state: \.counter2, action: \.counter2))
+            }
+        }
+        .buttonStyle(.borderless)
+        .navigationTitle("Two counters demo")
     }
 }
 
 #Preview {
-    TwoCounters()
+    TwoCounterView(store: Store(initialState: TwoCounters.State()) {
+        TwoCounters()
+    })
 }
