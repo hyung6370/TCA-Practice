f9bffc7d44d37c908af9ab05cbb60e1ec2ebae30
emptyhead <korea6370@yu.ac.kr>
2024-03-29 16:00:37 +0900

    24/3/29 update

---
+++ TCA-Practice-1/01/OptionalState.swift
@@ -0,0 +1,18 @@
+//
+//  OptionalState.swift
+//  TCA-Practice-1
+//
+//  Created by KIM Hyung Jun on 3/29/24.
+//
+
+import SwiftUI
+
+struct OptionalState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptionalState()
+}
---
+++ TCA-Practice-1/Internal/TemplateText.swift
@@ -0,0 +1,8 @@
+//
+//  TemplateText.swift
+//  TCA-Practice-1
+//
+//  Created by KIM Hyung Jun on 3/29/24.
+//
+
+import Foundation
