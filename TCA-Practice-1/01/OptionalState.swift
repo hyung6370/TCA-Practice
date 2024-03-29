b70802bd7f8d89e85172aa785e7acb3617c510fe
emptyhead <korea6370@yu.ac.kr>
2024-03-29 16:00:45 +0900

    24/3/29 update

--- TCA-Practice-1/01/OptionalState.swift
+++ TCA-Practice-1/01/OptionalState.swift
@@ -6,13 +6,102 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
-struct OptionalState: View {
+private let readMe = """
+  This screen demonstrates how to show and hide views based on the presence of some optional child \
+  state.
+
+  The parent state holds a `Counter.State?` value. When it is `nil` we will default to a plain \
+  text view. But when it is non-`nil` we will show a view fragment for a counter that operates on \
+  the non-optional counter state.
+
+  Tapping "Toggle counter state" will flip between the `nil` and non-`nil` counter states.
+  
+  
+  이 코드는 스유와 TCA를 사용하여 선택적 상태 관리의 예를 보여주는 프로그램이다.
+  여기서 optional state란 어떤 값이 있을 수도 있고, 없을 수도 있는 상태를 의미한다.
+  이 예제는 사용자가 UI의 버튼을 통해 특정 상태(여기서는 카운터)의 존재 여부를 토글할 수 있게 하며,
+  상태의 존재 여부에 따라 다른 뷰를 보여주는 방식으로 선택적 상태를 활용한다.
+  """
+
+@Reducer
+struct OptionalBasics {
+    @ObservableState
+    struct State: Equatable {
+        var optionalCounter: Counter.State? // optional State를 관리
+    }
+    
+    enum Action {
+        case optionalCounter(Counter.Action)
+        case toggleCounterButtonTapped
+    }
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .toggleCounterButtonTapped:
+                state.optionalCounter = state.optionalCounter == nil ? Counter.State() : nil
+                return .none
+            case .optionalCounter:
+                return .none
+            }
+        }
+        .ifLet(\.optionalCounter, action: \.optionalCounter) {
+            Counter()
+        }
+        // ifLet 연산자를 사용하여 optionalCounter가 nil이 아닐 때만 Counter 리듀서로 이벤트를 전달한다.
+    }
+}
+
+struct OptionalBasicsView: View {
+    let store: StoreOf<OptionalBasics>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                AboutView(readMe: readMe)
+            }
+            
+            Button("Toggle counter state") {
+                store.send(.toggleCounterButtonTapped)
+            }
+            
+            // optionalCounter 상태가 nil인지 아닌지에 따라 다른 텍스트 또는 CounterView를 보여준다.
+            if let store = store.scope(state: \.optionalCounter, action: \.optionalCounter) {
+                Text(template: "`Counter.State` is `nil`")
+                CounterView(store: store)
+                    .buttonStyle(.borderless)
+                    .frame(maxWidth: .infinity)
+            }
+            else {
+                Text(template: "`Counter.State` is `nil`")
+            }
+        }
+        .navigationTitle("Optional state")
     }
 }
 
 #Preview {
-    OptionalState()
+    NavigationStack {
+        OptionalBasicsView(
+            store: Store(initialState: OptionalBasics.State()) {
+                OptionalBasics()
+            }
+        )
+    }
+}
+
+#Preview("Deep-linked") {
+    NavigationStack {
+        OptionalBasicsView(store: Store(
+            initialState: OptionalBasics.State(
+                optionalCounter: Counter.State(
+                    count: 42
+                )
+            )
+        ) {
+            OptionalBasics()
+        })
+    }
 }
--- TCA-Practice-1/Internal/TemplateText.swift
+++ TCA-Practice-1/Internal/TemplateText.swift
@@ -5,4 +5,66 @@
 //  Created by KIM Hyung Jun on 3/29/24.
 //
 
-import Foundation
+import SwiftUI
+
+// Text 뷰를 확장하여 복잡한 스타일을 적용할 수 있는 기능을 추가한다.
+// 텍스트 내에서 특정문자를 사용하여 강조, 이텔릭, 코드 스타일 등을 적용할 수 있게 한다.
+// 이는 마크다운과 유사한 형태로, 텍스트 스타일을 선언적으로 정의할 수 있게 한다.
+extension Text {
+    init(template: String, _ style: Font.TextStyle = .body) {
+        enum Style: Hashable {
+            case code
+            case emphasis
+            case strong
+        }
+        
+        var segments: [Text] = []
+        var currentValue = ""
+        var currentStyles: Set<Style> = []
+        
+        func flushSegment() {
+            var text = Text(currentValue)
+            if currentStyles.contains(.code) {
+                text = text.font(.system(style, design: .monospaced))
+            }
+            if currentStyles.contains(.emphasis) {
+                text = text.italic()
+            }
+            if currentStyles.contains(.strong) {
+                text = text.bold()
+            }
+            segments.append(text)
+            currentValue.removeAll()
+        }
+        
+        for character in template {
+            switch character {
+            case "*":
+                flushSegment()
+                currentStyles.toggle(.strong)
+            case "_":
+                flushSegment()
+                currentStyles.toggle(.emphasis)
+            case "`":
+                flushSegment()
+                currentStyles.toggle(.code)
+            default:
+                currentValue.append(character)
+            }
+        }
+        flushSegment()
+        
+        self = segments.reduce(Text(verbatim: ""), +)
+    }
+}
+
+extension Set {
+    fileprivate mutating func toggle(_ element: Element) {
+        if self.contains(element) {
+            self.remove(element)
+        }
+        else {
+            self.insert(element)
+        }
+    }
+}
